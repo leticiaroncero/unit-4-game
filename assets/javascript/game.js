4cbbb49f33e15ca6ca15d6aca87f7d18141614b0
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-10-16 23:43:27 -0700

    add logic to assign value to each crystal and logic for wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,36 @@
-var randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+var wins = 0
+var losses = 0
+var winsCounter = $()
+var winsCounter
 
+var randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
 $("#target-number").text(randomNumber);
 
 var totalScore = 0;
-
 $("#total-score").text(totalScore);
 
-var crystalValue = [];
-
-var  imageCrystal = $(".crystal-image");
-
-
+var imageCrystal = $(".crystal-image");
 for (var i = 0; i < 4; i++) {
     randomValue = Math.floor(Math.random() * 12 + 1);
-    crystalValue.push(randomValue)
-    imageCrystal.attr("data-crystalvalue", crystalValue[i])
+    imageCrystal.eq(i).attr("data-crystalvalue", randomValue)
 }
 
 $(".crystal-image").on("click", function () {
     totalScore += $(this).attr("data-crystalvalue")
 
-    console.log(this)
-    console.log()
-    // if (totalScore == randomNumber) {
 
-    // }
+    if (totalScore === randomNumber) {
+        wins++
+        reset()
+    } else if (totalScore > randomNumber) {
+        losses++
+        reset()
+    }
 })
+
+function reset() {
+    var randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    $("#target-number").text(randomNumber);
+    var totalScore = 0;
+    $("#total-score").text(totalScore);
+}
